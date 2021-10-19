26aac990a7f2c25be3a6e7045389392760dcedc7
Just be free <justbefree@126.com>
2021-10-19 16:51:38 +0800

    docs: update

--- docs/.vuepress/themeConfig.js
+++ docs/.vuepress/themeConfig.js
@@ -2,28 +2,17 @@
 * @Author: Just be free
 * @Date:   2020-07-10 14:04:39
 * @Last Modified by:   Just be free
-* @Last Modified time: 2021-10-18 14:54:11
+* @Last Modified time: 2021-10-19 16:49:40
 * @E-mail: justbefree@126.com
 */
 module.exports = {
   nav: [
     { text: "Home", link: "/" },
     { text: "行程配置解读", link: "/trip/" },
-    // {
-    //   text: "components",
-    //   items: [
-    //     {
-    //       text: "@dullar/earth",
-    //       link: "/earth/"
-    //     },
-    //     {
-    //       text: "@dullar/mercury",
-    //       link: "/mercury/"
-    //     }
-    //   ]
-    // },
-    // { text: "BEM", link: "/bem/" },
-    // { text: "cli", link: "/cli/" }
+    {
+      text: "前端设计方案",
+      link: "/fe-design/"
+    },
   ],
   sidebar: {
     "/trip/": [
@@ -34,108 +23,18 @@ module.exports = {
           ["trip.md", "行程配置"]
         ]
       }
+    ],
+    "/fe-design/": [
+      {
+        title: "云商旅前端设计方案",
+        collapsable: true,
+        children: [
+          ["ai.md", "AI语音交互操作商旅"],
+          ["postmessage.md", "云商旅和ECS交互设计"],
+          ["sensors.md", "神策数据埋点设计方案"]
+        ]
+      }
     ]
-    // "/earth/": [
-    //   ["install.md", "Installation"],
-    //   {
-    //     title: "Basic components",
-    //     collapsable: true,
-    //     children: [
-    //       ["toast.md", "toast"],
-    //       ["indicator.md", "indicator"],
-    //       ["spin.md", "spin"],
-    //       ["iconfont.md", "iconfont"],
-    //       ["checkbox.md", "checkbox"],
-    //       ["radiobox.md", "radiobox"],
-    //       ["calendar.md", "calendar"],
-    //       ["popup.md", "popup"],
-    //       ["button.md", "button"],
-    //       ["flex.md", "flex"],
-    //       ["action-sheet.md", "action-sheet"],
-    //       ["field.md", "field"],
-    //       ["layout.md", "layout"],
-    //       ["city-picker.md", "city-picker"],
-    //       ["sticky.md", "sticky"],
-    //       ["dialog.md", "dialog"],
-    //       ["picky-stepper.md", "picky-stepper"],
-    //       ["dropdown.md", "dropdown"],
-    //       ["dropdown-menu.md", "dropdown-menu"],
-    //       ["picker.md", "picker"],
-    //       ["swipe.md", "swipe"],
-    //       ["tabs.md", "tabs"],
-    //       ["slider.md", "slider"],
-    //       ["submit-action.md", "submit-action"],
-    //       ["pull-refresh.md", "pull-refresh"],
-    //       ["date-picker.md", "date-picker"],
-    //       ["counter.md", "counter"],
-    //       ["skeleton.md", "skeleton"],
-    //       ["animation-list.md", "animation-list"],
-    //     ]
-    //   }
-    // ],
-    // "/mercury/": [
-    //   ["install.md", "Installation"],
-    //   {
-    //     title: "Basic components",
-    //     collapsable: true,
-    //     children: [
-    //       ["toast.md", "toast"],
-    //       ["indicator.md", "indicator"],
-    //       ["spin.md", "spin"],
-    //       ["iconfont.md", "iconfont"],
-    //       ["checkbox.md", "checkbox"],
-    //       ["radiobox.md", "radiobox"],
-    //       ["calendar.md", "calendar"],
-    //       ["popup.md", "popup"],
-    //       ["button.md", "button"],
-    //       ["flex.md", "flex"],
-    //       ["field.md", "field"],
-    //       ["layout.md", "layout"],
-    //       ["sticky.md", "sticky"],
-    //       ["dialog.md", "dialog"],
-    //       ["swipe.md", "swipe"],
-    //       ["tabs.md", "tabs"],
-    //       ["pull-refresh.md", "pull-refresh"],
-    //       ["counter.md", "counter"],
-    //       ["skeleton.md", "skeleton"],
-    //       ["animation-list.md", "animation-list"],
-    //       ["category.md", "category"],
-    //       ["magnifier.md", "magnifier"],
-    //       ["modal.md", "modal"],
-    //       ["cascader.md", "cascader"],
-    //       ["table.md", "table"],
-    //       ["shipping-address.md", "shipping-address"],
-    //       ["elastic-serach.md", "elastic-serach"],
-    //       ["category-mobile.md", "category-mobile"],
-    //       ["address.md", "address"],
-    //       ["badge.md", "badge"],
-    //       ["tooltip.md", "tooltip"],
-    //       ["input.md", "input"],
-    //       ["tree.md", "tree"],
-    //       ["slide.md", "slide"],
-    //       ["image.md", "image"],
-    //       ["form.md", "form"],
-    //     ]
-    //   }
-    // ],
-    // "/bem/": [],
-    // "/cli/": [
-    //   ["structure.md", "Directory structure"],
-    //   {
-    //     title: "Design principle",
-    //     collapsable: true,
-    //     children: [
-    //       ["before-start.md", "Before start"],
-    //       ["component.md", "Component"],
-    //       ["router.md", "Router"],
-    //       ["i18n.md", "I18n"],
-    //       ["store.md", "Store"],
-    //       ["export-application.md", "Application"],
-    //       ["vue-class-component.md", "Vue class component"]
-    //     ]
-    //   },
-    //   ["vue-property-decorator.md", "Vue property decorator"]
-    // ]
   },
   lastUpdated: "Last Updated",
   // Assumes GitHub. Can also be a full GitLab url.
